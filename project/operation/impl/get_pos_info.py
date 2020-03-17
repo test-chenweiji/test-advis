b6a318c2d42a73f28c59a2f3bc37c38a268f90c5
chenweiji <846725080@qq.com>
2020-03-18 05:50:15 +0800

    screen

--- project/operation/impl/get_pos_info.py
+++ project/operation/impl/get_pos_info.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-#coding:utf-8
 import sys
 import os
 import uuid
@@ -20,29 +19,30 @@ def query_pos():
     time1 = str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
     cursor.execute("SELECT uuid,screen_identifier,source_start,feature_id FROM pos where source_start > '%s'" % time1)
     result = cursor.fetchone()
-    pos_uuid = result[0]
-    screen = result[1]
-    for k, i in  enumerate(screen):
-        if i != '0':
-            _screen = screen[k:]
+    if result:
+        pos_uuid = result[0]
+        screen = result[1]
+        for k, i in enumerate(screen):
+            if i != '0':
+                _screen = screen[k:]
+                break
+        source_start = result[2]
+        start = str(source_start)
+        start = start.replace(' ', 'T')
+        feature_id = result[3]
 
-    source_start = result[2]
-    start = str(source_start)
-    start = start.replace(' ', 'T')
-    feature_id = result[3]
+        with open(root.get_root_path() + '/output/pos_info.txt', 'w') as f:
+            f.write(pos_uuid + '\n')
+            f.write(_screen + '\n')
+            f.write(start + '\n')
+            f.write(feature_id + '\n')
 
-    with open(root.get_root_path() + '/output/pos_info.txt', 'w') as f:
-        f.write(pos_uuid + '\n')
-        f.write(_screen + '\n')
-        f.write(start + '\n')
-        f.write(feature_id + '\n')
+    else:
+        print ('数据库查询POS数据为空或POS数据已过期，请添加POS后再运行')
+        sys.exit(0)
+
 
     connent.commit()
     connent.close()
-
-
-
-
-
 
 
